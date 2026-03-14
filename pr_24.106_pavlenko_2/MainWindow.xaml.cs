caa9f933e75aae191e92dbd85787b3fb195edd0e
JustChel883 <horeksid@mail.ru>
2026-03-14 10:47:43 +0700

    Добавьте файлы проекта.

---
+++ pr_24.106_pavlenko_2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace pr_24._106_pavlenko_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sentence = InputTextBox.Text;
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                MessageBox.Show("Введите предложение.");
+                return;
+            }
+
+            
+            string lower = sentence.ToLower();
+            int vowelCount = lower.Count(c => "aeiou".Contains(c));
+            VowelCountTextBlock.Text = vowelCount.ToString();
+
+            
+            string cleaned = new string(sentence.Select(c => char.IsLetter(c) ? c : ' ').ToArray());
+            string[] words = cleaned.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+            {
+                LongestWordTextBlock.Text = "нет слов";
+                return;
+            }
+
+            string longest = words.OrderByDescending(w => w.Length).First();
+            LongestWordTextBlock.Text = longest;
+        }
+    }
+}
