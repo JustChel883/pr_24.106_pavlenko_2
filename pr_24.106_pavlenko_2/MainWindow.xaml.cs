d0a83e48ae535c2874e39de8575bf6f262665cf3
JustChel883 <horeksid@mail.ru>
2026-03-14 11:15:06 +0700

    добавил типа try catch

--- pr_24.106_pavlenko_2/MainWindow.xaml.cs
+++ pr_24.106_pavlenko_2/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Windows;
 
@@ -13,39 +12,42 @@ namespace pr_24._106_pavlenko_2
             InitializeComponent();
         }
 
-        
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
-            string sentence = InputTextBox.Text;
-            if (string.IsNullOrWhiteSpace(sentence))
+            try
             {
-                MessageBox.Show("Введите предложение.");
-                return;
+                string sentence = InputTextBox.Text;
+                if (string.IsNullOrWhiteSpace(sentence))
+                {
+                    MessageBox.Show("Введите предложение.");
+                    return;
+                }
+                else if (Regex.IsMatch(sentence, @"[а-яА-ЯеЁ]"))
+                {
+                    MessageBox.Show("На английском!");
+                    return;
+                }
+
+                string lower = sentence.ToLower();
+                int vowelCount = lower.Count(c => "aeiou".Contains(c));
+                VowelCountTextBlock.Text = vowelCount.ToString();
+
+                string cleaned = new string(sentence.Select(c => char.IsLetter(c) ? c : ' ').ToArray());
+                string[] words = cleaned.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Length == 0)
+                {
+                    LongestWordTextBlock.Text = "нет слов";
+                    return;
+                }
+
+                string longest = words.OrderByDescending(w => w.Length).First();
+                LongestWordTextBlock.Text = longest;
             }
-            else if (Regex.IsMatch(sentence, @"[а-яА-ЯеЁ]"))
+            catch (Exception ex)
             {
-                MessageBox.Show("На английском!");
-                return;
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-         
-            
-            string lower = sentence.ToLower();
-            int vowelCount = lower.Count(c => "aeiou".Contains(c));
-            VowelCountTextBlock.Text = vowelCount.ToString();
-
-            
-            string cleaned = new string(sentence.Select(c => char.IsLetter(c) ? c : ' ').ToArray());
-            string[] words = cleaned.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            if (words.Length == 0)
-            {
-                LongestWordTextBlock.Text = "нет слов";
-                return;
-            }
-
-            string longest = words.OrderByDescending(w => w.Length).First();
-            LongestWordTextBlock.Text = longest;
         }
     }
 }
